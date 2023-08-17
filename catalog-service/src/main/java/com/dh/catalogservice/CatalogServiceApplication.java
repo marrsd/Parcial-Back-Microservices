a0aa1b5b5dc9188e326a3a77ff99bca84bcaa8c2
marrsd <mpenaranda1@misena.edu.co>
2023-08-16 21:55:46 -0500

    Agregado config client para eureka-server y catalog-service, agregado discovery client para catalog-service

--- catalog-service/src/main/java/com/dh/catalogservice/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/dh/catalogservice/CatalogServiceApplication.java
@@ -2,9 +2,10 @@ package com.dh.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CatalogServiceApplication {
 
     public static void main(String[] args) {
