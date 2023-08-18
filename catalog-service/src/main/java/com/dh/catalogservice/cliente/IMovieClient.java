ea1650051ba71263e6afb8272d80c303371f29c9
Tatiana García <86701915+Tatianag2@users.noreply.github.com>
2023-08-17 21:14:52 -0500

    Se implementa balanceo de carga con random

--- catalog-service/src/main/java/com/dh/catalogservice/cliente/IMovieClient.java
+++ catalog-service/src/main/java/com/dh/catalogservice/cliente/IMovieClient.java
@@ -2,6 +2,7 @@ package com.dh.catalogservice.cliente;
 
 import java.util.List;
 
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +10,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.dh.catalogservice.FeignConfiguration;
 import com.dh.catalogservice.model.Movie;
 
 @FeignClient(name = "movie-service")
+@LoadBalancerClient(name = "movie-service", configuration = FeignConfiguration.class)
 public interface IMovieClient {
 
   @GetMapping("/api/v1/movies/{genre}")
