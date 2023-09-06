c2f3b4edff54ceab05ffa2f347da8542cce17dc9
Tatiana García <86701915+Tatianag2@users.noreply.github.com>
2023-09-06 00:45:58 -0500

    Se adiciona a Feign método obtener por género para Serie

--- catalog-service/src/main/java/com/dh/catalogservice/cliente/ISerieClient.java
+++ catalog-service/src/main/java/com/dh/catalogservice/cliente/ISerieClient.java
@@ -1,8 +1,12 @@
 package com.dh.catalogservice.cliente;
 
+import java.util.List;
+
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -12,6 +16,9 @@ import com.dh.catalogservice.model.Serie;
 @FeignClient(name = "serie-service")
 @LoadBalancerClient(name = "serie-service", configuration = FeignConfiguration.class)
 public interface ISerieClient {
+	
+	  @GetMapping("/api/v1/series/{genre}")
+	  ResponseEntity<List<Serie>> getSerieByGenre(@PathVariable String genre);
 	
 	  @PostMapping("/api/v1/series/save")
 	  ResponseEntity<Serie> saveSerie(@RequestBody Serie serie);
--- catalog-service/src/main/java/com/dh/catalogservice/controller/CatalogController.java
+++ catalog-service/src/main/java/com/dh/catalogservice/controller/CatalogController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dh.catalogservice.cliente.IMovieClient;
@@ -16,6 +17,7 @@ import com.dh.catalogservice.model.Movie;
 import com.dh.catalogservice.model.Serie;
 
 @RestController
+@RequestMapping("/catalog")
 public class CatalogController {
 
   @Autowired
@@ -24,20 +26,28 @@ public class CatalogController {
   @Autowired
   private ISerieClient iSerieClient;
 
-  @GetMapping("catalog/{genre}")
-  public ResponseEntity<List<Movie>> getCatalogByGenre(@PathVariable String genre) {
+  @GetMapping("/movie/{genre}")
+  public ResponseEntity<List<Movie>> getCatalogByGenreMovie(@PathVariable String genre) {
     ResponseEntity<List<Movie>> response = iMovieClient.getMovieByGenre(genre);
     
-    System.out.println("Instancia con id: " + response.getHeaders().get("idrandom"));
+    System.out.println("Instancia movie con id: " + response.getHeaders().get("idrandom"));
+    return response;
+  }
+  
+  @GetMapping("/serie/{genre}")
+  public ResponseEntity<List<Serie>> getCatalogByGenreSerie(@PathVariable String genre) {
+    ResponseEntity<List<Serie>> response = iSerieClient.getSerieByGenre(genre);
+    
+    System.out.println("Instancia serie con id: " + response.getHeaders().get("idrandom"));
     return response;
   }
 
-  @PostMapping("catalog/saveMovie")
+  @PostMapping("/saveMovie")
   public ResponseEntity<Movie> saveMovie(@RequestBody Movie movie) {
     return iMovieClient.saveMovie(movie);
   }
   
-  @PostMapping("catalog/saveSerie")
+  @PostMapping("/saveSerie")
   public ResponseEntity<Serie> saveSerie(@RequestBody Serie serie) {
     return iSerieClient.saveSerie(serie);
   }
